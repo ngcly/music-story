906560a752533225eeb174460af83de1886924e1
cly00 <531237716@qq.com>
2018-06-19 17:09:10 +0800

    版本更新

--- README.md
+++
@@ -1,7 +0,0 @@
-# music-story
-
-> 音乐故事前端
-
-## Build Setup
-
-npm run serve
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,7 @@
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank">unit-jest</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -31,15 +32,15 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
 h3 {
   margin: 40px 0 0;
 }
