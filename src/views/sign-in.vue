c13550ded37bfa0fa8d10e4037f6bfc6773c8fab
ngcly <531237716@qq.com>
2018-12-29 16:54:29 +0800

    修改样式

--- src/views/sign-in.vue
+++ src/views/sign-in.vue
@@ -15,52 +15,50 @@
                     <router-link to="/sign-up">注册</router-link>
                 </h4>
             </div>
-            <div class="js-sign-in">
-                <form method="post">
-                    <div class="input-prepend ">
-                        <input class="top-radius" type="text"  placeholder="手机号或邮箱" v-model="loginForm.username">
-                        <i class="fa fa-user"></i>
-                    </div>
-                    <div class="input-prepend">
-                        <input class="bottom-radius" type="password"  placeholder="密码" v-model="loginForm.password">
-                        <i class="fa fa-lock"></i>
-                    </div>
-                    <div class="remember">
-                        <input type="checkbox">&nbsp;
-                        <span>记住我</span>
-                    </div>
-                    <div class="help">
-                        <router-link to="/help">
-                            登录遇到问题?
-                        </router-link>
-                    </div>
-                    <input class="btn btn-primary" type="button" value="登录" @click="login">
-                </form>
-                <div class="more-sign">
-                    <p>社交账号登录</p>
-                    <ul>
-                        <li class="weibo">
-                            <a href="#">
-                                <i class="fa fa-weibo"></i>
-                            </a>
-                        </li>
-                        <li class="weixin">
-                            <a href="#">
-                                <i class="fa fa-weixin"></i>
-                            </a>
-                        </li>
-                        <li class="qq">
-                            <a href="#">
-                                <i class="fa fa-qq"></i>
-                            </a>
-                        </li>
-                        <li class="other">
-                            <a href="#">
-                                其他
-                            </a>
-                        </li>
-                    </ul>
+            <el-form :model="loginForm" :rules="rules" ref="loginForm">
+                <el-form-item prop="username" class="input-prepend">
+                    <el-input class="top-radius" placeholder="手机号或邮箱" v-model="loginForm.username"></el-input>
+                    <i class="fa fa-user"></i>
+                </el-form-item>
+                <el-form-item prop="password" class="input-prepend">
+                    <el-input class="bottom-radius" type="password"  placeholder="密码" v-model="loginForm.password"></el-input>
+                    <i class="fa fa-lock"></i>
+                </el-form-item>
+                <div class="remember">
+                    <input type="checkbox">&nbsp;
+                    <span>记住我</span>
                 </div>
+                <div class="help">
+                    <router-link to="/help">
+                        登录遇到问题?
+                    </router-link>
+                </div>
+                <input class="btn btn-primary" type="button" value="登录" @click="login('loginForm')">
+            </el-form>
+            <div class="more-sign">
+                <p>社交账号登录</p>
+                <ul>
+                    <li class="weibo">
+                        <a href="#">
+                            <i class="fa fa-weibo"></i>
+                        </a>
+                    </li>
+                    <li class="weixin">
+                        <a href="#">
+                            <i class="fa fa-weixin"></i>
+                        </a>
+                    </li>
+                    <li class="qq">
+                        <a href="#">
+                            <i class="fa fa-qq"></i>
+                        </a>
+                    </li>
+                    <li class="other">
+                        <a href="#">
+                            其他
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -73,16 +71,30 @@
                 loginForm: {
                     username:'',
                     password:''
+                },
+                rules: {
+                    username: [
+                        {required: true,message: '请输入用户名',trigger: 'blur'}
+                    ],
+                    password: [
+                        {required: true,message: '请输入密码',trigger: 'blur'}
+                    ]
                 }
             }
         },
         methods: {
-            login(){
-                this.$store.dispatch("Login",this.loginForm)
-                .then(() => {
-                    this.loading = false;
-                    this.$router.push(this.$route.query.redirect || '/home')
-                }).catch(() => {this.loading = false;});
+            login(formName){
+                this.$refs[formName].validate((valid)=>{
+                    if(valid){
+                        this.$store.dispatch("Login",this.loginForm)
+                        .then(() => {
+                            this.loading = false;
+                            this.$router.push(this.$route.query.redirect || '/home')
+                            }).catch(() => {this.loading = false;});
+                    }else{
+                        return false;
+                    }
+                });
             }
         }
     }
--- src/views/sign-up.vue
+++ src/views/sign-up.vue
@@ -15,45 +15,43 @@
                     <router-link class="active" to="/sign-up">注册</router-link>
                 </h4>
             </div>
-            <div class="js-sign-up">
-                <form method="post">
-                    <div class="input-prepend">
-                        <input class="top-radius" type="text" value="" name="" placeholder="你的昵称" v-model="signupForm.nickname">
-                        <i class="fa fa-user"></i>
-                    </div>
-                    <div class="input-prepend">
-                        <input type="text" value="" name="" placeholder="手机号">
-                        <i class="fa fa-mobile-phone"></i>
-                    </div>
-                    <div class="input-prepend ssm">
-                        <input type="text" name="" value="" placeholder="手机验证码">
-                        <i class="fa fa-shield"></i>
-                        <a class="send-ssm btn-success" href="#">发送验证码</a>
-                    </div>
-                    <div class="input-prepend">
-                        <input class="bottom-radius" type="password" value="" name="" placeholder="设置密码" v-model="signupForm.password">
-                        <i class="fa fa-lock"></i>
-                    </div>
-                    <input class="btn btn-success sign-up-btn" type="button" value="注册">
-                    <p class="sign-up-msg">
-                        点击 “注册” 即表示您同意并愿意遵守音书<br>用户协议和隐私政策
-                    </p>
-                </form>
-                <div class="more-sign">
-                    <p>社交账号直接登录</p>
-                    <ul>
-                        <li class="weixin">
-                            <a href="#">
-                                <i class="fa fa-weixin"></i>
-                            </a>
-                        </li>
-                        <li class="qq">
-                            <a href="#">
-                                <i class="fa fa-qq"></i>
-                            </a>
-                        </li>
-                    </ul>
+            <el-form :model="signupForm" :rules="rules" ref="signupForm">
+                <el-form-item class="input-prepend" prop="nickName">
+                    <el-input class="top-radius" placeholder="你的昵称" v-model="signupForm.nickname"></el-input>
+                    <i class="fa fa-user"></i>
+                </el-form-item>
+                <el-form-item class="input-prepend" prop="phone">
+                    <el-input placeholder="用户名" v-model="signupForm.username"></el-input>
+                    <i class="fa fa-mobile-phone"></i>
+                </el-form-item>
+                <div class="input-prepend ssm">
+                    <input type="text" name="" value="" placeholder="手机验证码">
+                    <i class="fa fa-shield"></i>
+                    <a class="send-ssm btn-success" href="#">发送验证码</a>
                 </div>
+                <div class="input-prepend">
+                    <input class="bottom-radius" type="password" value="" name="" placeholder="设置密码" v-model="signupForm.password">
+                    <i class="fa fa-lock"></i>
+                </div>
+                <input class="btn btn-success sign-up-btn" type="button" value="注册">
+                <p class="sign-up-msg">
+                    点击 “注册” 即表示您同意并愿意遵守音书<br>用户协议和隐私政策
+                </p>
+            </el-form>
+            <div class="more-sign">
+                <p>社交账号直接登录</p>
+                <ul>
+                    <li class="weixin">
+                        <a href="#">
+                            <i class="fa fa-weixin"></i>
+                        </a>
+                    </li>
+                    <li class="qq">
+                        <a href="#">
+                            <i class="fa fa-qq"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -68,6 +66,14 @@
                     password:'',
                     email:'',
                     nickname:''
+                },
+                rules: {
+                    username: [
+                        {required: true,message: '请输入用户名',trigger: 'blur'}
+                    ],
+                    password: [
+                        {required: true,message: '请输入密码',trigger: 'blur'}
+                    ]
                 }
             }
         },
