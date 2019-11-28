65d00ba0a223281a047c7e70287dde4567087cf2
ngcly <531237716@qq.com>
2019-11-28 22:50:02 +0800

    图片压缩

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,35 +1,37 @@
 const CompressionPlugin = require('compression-webpack-plugin')
-var webpack=require('webpack');
 
 module.exports = {
-    chainWebpack: (config) => {
-    /* 添加分析工具*/
-        if (process.env.NODE_ENV === 'production') {
-            if (process.env.npm_config_report) {
-                config
-                    .plugin('webpack-bundle-analyzer')
-                    .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
-                    .end();
-                config.plugins.delete('prefetch')
-            }
+    publicPath: './',
+    chainWebpack: config => {
+      // 解决ie11兼容ES6
+      config.entry('main').add('babel-polyfill')
+      // 开启图片压缩
+      config.module.rule('images')
+      .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({ bypassOnDebug: true })
+      // 开启js、css压缩
+      if (process.env.NODE_ENV === 'production') {
+        config.plugin('compressionPlugin')
+        .use(new CompressionPlugin({
+          test:/\.js$|\.html$|.\css/, // 匹配文件名
+          threshold: 10240, // 对超过10k的数据压缩
+          deleteOriginalAssets: false // 不删除源文件
+        }))
+      }
+    },
+
+    configureWebpack: {
+        externals: {
+            'jquery':'$'
         }
     },
-    configureWebpack: (config) => {
-        if(process.env.NODE_ENV === 'production'){
-            // 为生产环境修改配置...
-            config.mode = 'production'
-            return {
-                plugins: [new CompressionPlugin({
-                    test: /\.js$|\.html$|\.css/, //匹配文件名
-                    threshold: 10240, //对超过10k的数据进行压缩
-                    deleteOriginalAssets: false //是否删除原文件
-                }),
-                new webpack.ProvidePlugin({
-                    'jquery':'$'
-                })]
-            }
-        }
-    },
+
+    transpileDependencies: [
+        'biyi-admin', // 指定对第三方依赖包进行babel-polyfill处理
+    ],
+
     devServer:{//代理
         port:9091,
         proxy:'http://127.0.0.1:8082'
